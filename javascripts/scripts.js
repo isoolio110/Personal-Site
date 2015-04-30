4129c7e4a4739e4dd6fa81dfc5977dc35c4293b8
isoolio110 <kim.isul@gmail.com>
2015-04-30 16:41:34 -0400

    Adds sliding animation scripts

---
+++ javascripts/scripts.js
@@ -0,0 +1,52 @@
+$(function(){
+
+  $("#landing-pg-header").animate({ 
+      top: "+=200px",
+    }, 1000 );    
+  
+  $("#landing-pg-subtitle").animate({
+      top: "+=290px",
+    }, 1400 );
+
+  var width = $(".fade-in-left").width()  
+  $(".fade-in-left").css({
+    right: "-" + (width + 200) + "px"})
+
+  var width = $(".fade-in-right").width()  
+  $(".fade-in-right").css({
+    left: "-" + (width+ 200) + "px"})
+
+  function slideElementSideways(id, way, speed, offset) {
+    var elm = document.getElementById(id)
+    var jelm = $(elm)
+    if (way === "right") {
+      jelm.waypoint(function(direction){
+        var width = jelm.width();
+        jelm.animate({right: 0 + "px"}, speed);
+      }, {
+        offset: offset
+      });      
+    }
+    if (way === "left") {
+      jelm.waypoint(function(direction){
+        var width = jelm.width();
+        jelm.animate({left: 0 + "px"}, speed);
+      }, {
+        offset: offset
+      });
+    }
+  };
+
+  slideElementSideways("isul-kim-summary-div", 
+    "right", 1000, 400);
+  slideElementSideways("MusicMap", 
+    "right", 1000, 400);
+  slideElementSideways("GlemurGames", 
+    "right", 1000, 400);
+  slideElementSideways("PerSee", 
+    "left", 1000, 400);
+  slideElementSideways("Globetrotters", 
+    "left", 1000, 400);
+
+
+});
